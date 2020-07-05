a526d68e37a4a1cc9dbb1868f33ffd2b66a30a7a
Gregor Hyneck <gregor@hyneck.de>
2020-07-05 15:49:40 +0200

    Restructure trails and visibility

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,38 +1,20 @@
 import React, { useEffect } from "react";
 import { hot } from 'react-hot-loader/root'
-import TrailMap from "../MapView/TrailMap";
-import VisibleMarkers from "../MapView/VisibleMarkers";
 import { loadTrails } from "../../redux/slices/trail";
-import { useTypedSelector, useTypedDispatch } from "../../redux";
+import { useTypedDispatch } from "../../redux";
 import './styles.module.scss';
+import MapView from "../MapView/MapView";
+
 
 const App : React.FunctionComponent = () => {
     const dispatch = useTypedDispatch();
-    const loading = useTypedSelector((state) => state.trails.loading);
-    const error = useTypedSelector((state) => state.trails.error);
-    const trailVisibility = useTypedSelector((state) => state.trails.trailVisibility);
-    const trails = useTypedSelector((state) => state.trails.trails);
 
     useEffect(() => {
         dispatch(loadTrails());
     }, [dispatch])
 
     return (
-        <>
-            {trails && trails.length ? (
-                <>
-                  <TrailMap trails={trails} />
-                  <VisibleMarkers trails={trails} trailVisibility={trailVisibility}/>
-                </>
-                ) : loading ? (
-                <b>Loading Trails</b>
-            ) : (
-                <b>Error loading Trails: {error}</b>
-            )
-            }
-
-        </>
-
+        <MapView />
     )
 }
 
--- src/components/MapView/TrailMap.tsx
+++ src/components/MapView/TrailMap.tsx
@@ -1,11 +1,9 @@
-import React, {useRef, useEffect} from "react";
+import React, {useRef, useEffect, useState, useCallback} from "react";
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
 import L from "leaflet";
-import { setVisibility } from "../../redux/slices/trail";
+import {Trail} from "../../redux/slices/trail";
 import "../../../node_modules/leaflet/dist/leaflet.css";
-import { Trail } from "../../redux/slices/trail";
 import { LeafletEvent } from "leaflet";
-import { useDispatch } from 'react-redux'
 
 export const pointerIcon = new L.Icon({
     iconUrl: require('../../../node_modules/leaflet/dist/images/marker-icon.png'),
@@ -17,33 +15,30 @@ export const pointerIcon = new L.Icon({
 })
 
 type props = {
-    trails: Array<Trail>
+    trails: Trail[]
+    showTrail: (trailId: number) => void
+    hideTrail: (trailId: number) => void
 }
 
-const TrailMap: React.FunctionComponent<props> = ({trails}) => {
+const TrailMap: React.FunctionComponent<props> = ({trails, showTrail, hideTrail}) => {
     const mapReference = useRef(null);
-    const dispatch = useDispatch()
+    const [lat] = useState(47.9959)
+    const [lng] = useState(7.85222)
+    const [zoom] = useState(13)
 
-
-    const initialPositionOfMap = {
-        lat: 47.9959,
-        lng: 7.85222,
-        zoom: 10
-    }
-
-    const updateTrailVisibility = (map: Map) => {
+    const updateTrailVisibility = useCallback((map: Map) => {
         trails.forEach(trail => {
             // @ts-ignore: Unreachable code error
             if (map.getBounds().contains(trail.position)) {
-                dispatch(setVisibility(trail.id, true));
+                showTrail(trail.id)
             } else {
-                dispatch(setVisibility(trail.id, false));
+                hideTrail(trail.id)
             }
         });
-    }
+    }, [showTrail, hideTrail, trails])
 
     const handleMoveEnd = (event: LeafletEvent) => {
-        updateTrailVisibility(event.target);
+        updateTrailVisibility(event.target)
     }
 
     useEffect(() => {
@@ -52,15 +47,16 @@ const TrailMap: React.FunctionComponent<props> = ({trails}) => {
             updateTrailVisibility(mapReference.current.leafletElement);
         }
 
-    })
+    }, [mapReference, updateTrailVisibility]
+    )
 
     return (
         <Map
             ref={mapReference}
             style={{ height: "280px", width: "100%" }}
-            zoom={initialPositionOfMap.zoom}
+            zoom={zoom}
             onMoveEnd={handleMoveEnd}
-            center={[initialPositionOfMap.lat, initialPositionOfMap.lng]}>
+            center={[lat, lng]}>
             <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
 
             {trails && trails.length ? trails.map((trail) =>
