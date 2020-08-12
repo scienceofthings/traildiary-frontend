ba0cd13f3f81d2ad39c9b457749de08bce5815b0
Gregor Hyneck <supergreg@gmx.de>
2020-08-12 21:11:51 +0200

    Add wouter and three pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react'
 import { hot } from 'react-hot-loader/root'
+import { Route } from 'wouter'
 import { loadTrails } from '../../redux/slices/trail'
 import { useTypedDispatch } from '../../redux'
-import './styles.module.scss'
 import Map from '../pages/Map/Map'
+import List from '../pages/List/List'
+import './App.module.scss'
+import Detail from '../pages/Detail/Detail'
+import Navigation from '../Common/Navigation/Navigation'
 
 const App: React.FunctionComponent = () => {
   const dispatch = useTypedDispatch()
@@ -12,7 +16,20 @@ const App: React.FunctionComponent = () => {
     dispatch(loadTrails())
   }, [dispatch])
 
-  return <Map />
+  return (
+    <>
+      <Navigation />
+      <Route path="/">
+        <Map />
+      </Route>
+      <Route path="/list">
+        <List />
+      </Route>
+      <Route path="/detail/:trailId">
+        {(params) => <Detail trailId={parseInt(params.trailId)} />}
+      </Route>
+    </>
+  )
 }
 
 export default hot(App)
