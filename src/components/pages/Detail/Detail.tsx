026eab61d463258084a95669dce20a86c2921dbd
Gregor Hyneck <supergreg@gmx.de>
2021-02-01 20:59:17 +0100

    Add Map Detail Page

--- src/components/pages/Detail/Detail.tsx
+++ src/components/pages/Detail/Detail.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Trail } from '../../../redux/slices/trail'
+import { Button } from 'react-bootstrap'
+import DetailMap from './DetailMap'
 
 type DetailProps = {
-  trailId: number | undefined
+  trail: Trail | undefined
 }
 
-const Detail: React.FunctionComponent<DetailProps> = ({ trailId }) => {
-  return <>Detail: {trailId}</>
+const Detail: React.FunctionComponent<DetailProps> = ({ trail }) => {
+  if (trail === undefined) return <></>
+  return (
+    <>
+      <h1>{trail.title}</h1>
+      <DetailMap trail={trail} />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: trail.description,
+        }}
+      ></div>
+      <div>
+        <a href={trail.gpxFile}>
+          <Button>Download</Button>
+        </a>
+      </div>
+    </>
+  )
 }
 
 export default Detail
