32208f135b7c5a649b19a7957fdf00a3e8ef2e23
Gregor Hyneck <supergreg@gmx.de>
2021-02-05 20:06:52 +0100

    Show responsive images in detail page

--- src/components/pages/Detail/Detail.tsx
+++ src/components/pages/Detail/Detail.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Trail } from '../../../redux/slices/trail'
-import { Button } from 'react-bootstrap'
+import { Button, Col, Row } from 'react-bootstrap'
 import DetailMap from './DetailMap'
+
+import styles from './Detail.module.scss'
+import ImageSection from './ImageSection'
 
 type DetailProps = {
   trail: Trail | undefined
@@ -9,20 +12,36 @@ type DetailProps = {
 
 const Detail: React.FunctionComponent<DetailProps> = ({ trail }) => {
   if (trail === undefined) return <></>
+
   return (
     <>
-      <h1>{trail.title}</h1>
-      <DetailMap trail={trail} />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: trail.description,
-        }}
-      ></div>
-      <div>
-        <a href={trail.gpxFile}>
-          <Button>Download</Button>
-        </a>
-      </div>
+      <Row className={styles.ghy}>
+        <Col>
+          <DetailMap trail={trail} />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1>{trail.title}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: trail.description,
+            }}
+          />
+        </Col>
+      </Row>
+      {trail.images.length > 0 && <ImageSection images={trail.images} />}
+      <Row>
+        <Col>
+          <a href={trail.gpxFile}>
+            <Button>Download GPX</Button>
+          </a>
+        </Col>
+      </Row>
     </>
   )
 }
