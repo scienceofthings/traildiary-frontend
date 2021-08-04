02274bdf6f0649cfbfa266243e41be5c2470f7f8
Gregor Hyneck <supergreg@gmx.de>
2021-08-04 22:11:59 +0200

    Fix detail trail URL

--- src/api/trail.ts
+++ src/api/trail.ts
@@ -2,5 +2,5 @@ import {config} from "../config";
 
 export const getTrailsEndpoint = (): string => (`${config.apiUrlPrefix}/trails/`)
 export const getTrailEndpoint = (trailId: number): string => (
-    `${getTrailsEndpoint()}/${trailId}`
+    `${getTrailsEndpoint()}${trailId}/`
 )
